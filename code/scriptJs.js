a94f75beedd7579c34912fd5a6a25b84680b9b4d
Bran-Marchaland <83598421+Bran-Marchaland@users.noreply.github.com>
2022-11-17 15:04:59 +0100

    Début du projet
    Ajouts des premier fichier

---
+++ code/scriptJs.js
@@ -0,0 +1,22 @@
+action1 = document.getElementById("action_1");
+action2 = document.getElementById("action_2");
+action3 = document.getElementById("action_3");
+action4 = document.getElementById("action_4");
+action5 = document.getElementById("action_5");
+
+
+action1.onclick = function(){
+    document.getElementById("ecranP").style.background = "black";
+}
+action2.onclick = function(){
+    document.getElementById("ecranP").style.background = "yellow";
+}
+action3.onclick = function(){
+    document.getElementById("ecranP").style.background = "blue";
+}
+action4.onclick = function(){
+    document.getElementById("ecranP").style.background = "orange";
+}
+action5.onclick = function(){
+    document.getElementById("ecranP").style.background = "green";
+}
